cd1c423a827de4243221a9b35ce5b6dcf38880c1
Asakitan <Asakitan@users.noreply.github.com>
2026-06-04 18:27:36 +1000

    fix

--- Start.bat
+++ Start.bat
@@ -1,2 +1,10 @@
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Readmin...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
+cd /d "%~dp0"
 python main.py
 pause
