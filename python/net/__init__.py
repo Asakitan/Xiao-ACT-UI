74cbfb59d3c91135e2264811628e9c455e4bce4e
Asakitan <Asakitan@users.noreply.github.com>
2026-06-16 04:04:24 +1000

    5.0.0-p5e-prep: remove platform shim files
    删除 Phase 5A 创建的 shim __init__.py:
    - python/proto/__init__.py
    - python/net/__init__.py
    - python/packet_parser/__init__.py
    
    原则: 平台零依赖插件。不在平台目录放任何指向插件的 shim/redirect。
    游戏专属代码的调用方如果在平台文件里，那段代码本身需要搬到插件。

--- python/net/__init__.py
+++
@@ -1,5 +0,0 @@
-# Shim: net content moved to plugins/star_resonance_plugin/net/
-try:
-    from plugins.star_resonance_plugin.net import *  # noqa: F401,F403
-except ImportError:
-    pass
--- python/packet_parser/__init__.py
+++
@@ -1,5 +0,0 @@
-# Shim: packet_parser content moved to plugins/star_resonance_plugin/protocol/
-try:
-    from plugins.star_resonance_plugin.protocol import *  # noqa: F401,F403
-except ImportError:
-    pass
